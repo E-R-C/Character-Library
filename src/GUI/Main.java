6e8097c712c71fd7fc7889ecebb00fd46c4e687c
SquirreLJ <leslieJD@hendrix.edu>
2015-10-27 21:17:03 -0500

    Here's the GUI and an update to the Main function so it'll run.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../GUI/GUI.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 805, 400));
         primaryStage.show();
     }
 
