2af2969e27ac9f767678cec7250234c572cc7428
E-R-C <ehuynh.school@gmail.com>
2015-10-29 22:25:33 -0500

    Database Update with untested load, edit and delete of Characters and Items. Database.java created to deal with all database calls

--- src/Data/Character.java
+++ src/Data/Character.java
@@ -4,4 +4,12 @@ package Data;
  * Created by Eric on 10/25/2015.
  */
 public class Character {
+    int ID;
+
+    public int getID(){
+        return ID;
+    }
+    public void update(String column, String new_value){
+
+    }
 }
