1ae363716c7c294eb43d1637d8498e3b54787db7
vXenonv <devin.henderson@rocketmail.com>
2015-11-01 19:35:38 -0600

    Devin's Character & Item Classes

--- src/Data/Character.java
+++ src/Data/Character.java
@@ -3,13 +3,66 @@ package Data;
 /**
  * Created by Eric on 10/25/2015.
  */
-public class Character {
-    int ID;
 
-    public int getID(){
-        return ID;
-    }
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Character {
+	
+    private StringProperty name;
+    private StringProperty age;
+    private StringProperty gender;
+    private StringProperty race;
+    private StringProperty occupation;
+    private StringProperty cID;
+    
     public void update(String variabletochange, String new_value){
 
     }
+    
+    public Character(String name){
+    	this.name = new SimpleStringProperty(name);
+    	age = new SimpleStringProperty("N/A");
+    	gender = new SimpleStringProperty("N/A");
+    	race = new SimpleStringProperty("N/A");
+    	occupation = new SimpleStringProperty("N/A");
+    	cID = new SimpleStringProperty();
+    }
+    
+    public void setName(String word){
+    	name.set(word);
+    }
+    
+    public void setAge(String word){
+    	age.set(word);
+    }
+    
+    public void setGender(String word){
+    	gender.set(word);
+    }
+    
+    public void setRace(String word){
+    	race.set(word);
+    }
+    
+    public void setOccupation(String word){
+    	occupation.set(word);
+    }
+    
+    public void setcID(String c){
+    	cID.set(c);
+    }
+    
+    public String getName() {return name.get();} 
+	public StringProperty getNameProperty() {return name;}
+	public String getAge() {return age.get();}
+	public StringProperty getAgeProperty() {return age;}
+	public String getGender() {return gender.get();}
+	public StringProperty getGenderProperty() {return gender;}
+	public String getRace() {return race.get();} 
+	public StringProperty getRaceProperty() {return race;}
+	public String getOccupation() {return occupation.get();}
+	public StringProperty getOccupationProperty() {return occupation;}
+	public String getcID() {return cID.get();}
+	public StringProperty getcIDProperty() {return cID;}
 }
--- src/Data/Item.java
+++ src/Data/Item.java
@@ -3,17 +3,62 @@ package Data;
 /**
  * Created by Eric on 10/25/2015.
  */
-public class Item {
-    int ID;
 
-    public int getID(){
-        return ID;
-    }
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Item {
+    
+    private StringProperty name;
+    private StringProperty location;
+    private StringProperty owner;
+    private StringProperty iID;
+    private StringProperty oID;
+    
 
     public void update(String variabletochange, String new_val){
 
     }
     public void changeOwner(){
 
+    }    
+    
+    public Item(String name){
+    	this.name = new SimpleStringProperty(name);
+    	location = new SimpleStringProperty("N/A");
+    	owner = new SimpleStringProperty("None");
+    	iID = new SimpleStringProperty();
+    	oID = new SimpleStringProperty();
     }
+    
+    public void setName(String word){
+    	name.set(word);
+    }
+    
+    public void setLocation(String word){
+    	location.set(word);
+    }
+    
+    public void setOwner(String word){
+    	owner.set(word);
+    }
+   
+    public void setiID(String i){
+    	iID.set(i);
+    }
+    
+    public void setoID(String o){
+    	oID.set(o);
+    }
+    
+    public String getName() {return name.get();} 
+	public StringProperty getNameProperty() {return name;}
+	public String getLocation() {return location.get();}
+	public StringProperty getLocationProperty() {return location;}
+	public String getOwner() {return owner.get();}
+	public StringProperty getOwnerProperty() {return owner;}
+	public String getiID() {return iID.get();}
+	public StringProperty getiIDProperty() {return iID;}
+	public String getoID() {return oID.get();}
+	public StringProperty getoIDProperty() {return oID;}
 }
