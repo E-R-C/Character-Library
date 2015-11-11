336d79bc805c1d902893ff2e08d2387df2fb6a32
E-R-C <ehuynh.school@gmail.com>
2015-11-11 09:41:33 -0600

    :lollipop: Database Organization

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -27,7 +27,7 @@ public class Database {
         filter_character = FXCollections.observableArrayList();
         filter_Event = FXCollections.observableArrayList();
         Class.forName("org.sqlite.JDBC");
-        why();
+        openConnection();
         con = DriverManager.getConnection("jdbc:sqlite:story.db");
         stat = con.createStatement();
         String cmd = "CREATE TABLE IF NOT EXISTS CHARACTERS (Name VARCHAR, Gender VARCHAR, Age VARCHAR, Race VARCHAR, Occupation VARCHAR, CharacterID VARCHAR);";
@@ -38,7 +38,7 @@ public class Database {
         execute(cmd);
         populate_lists();
     }
-    private void why() throws SQLException {
+    private void openConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
@@ -48,53 +48,47 @@ public class Database {
         con = DriverManager.getConnection("jdbc:sqlite:story.db");
         stat = con.createStatement();
     }
-    private void because() throws SQLException{
+    private void closeConnection() throws SQLException{
         stat.close();
         con.close();
     }
 
     public void populate_lists() throws SQLException {
-        why();
+        openConnection();
         ResultSet rs = stat.executeQuery( "SELECT * FROM CHARACTERS;" );
         while(rs.next()){
             character_list.add(new Character(rs.getString("Name"), rs.getString("Gender"), rs.getString("Age"), rs.getString("Race"), rs.getString("Occupation"), rs.getString("CharacterID")));
-            // rs.getString("Name"), rs.getString("Age"), rs.getString("Gender"), rs.getString("Race"), rs.getString("Occupation"), rs.getString("CharcterID")
             }
         System.out.println(character_list.size());
         rs = stat.executeQuery("SELECT * FROM ITEMS;");
         while(rs.next()){
             item_list.add(new Item(rs.getString("Name"), rs.getString("Location"), rs.getString("Owner"), rs.getString("CharacterID"), rs.getString("ItemID")));
-            // rs.getString("Name"),rs.getString("Location"),rs.getString("Owner"), rs.getString("CharacterID"), rs.getString("ItemID")
             }
         rs = stat.executeQuery("SELECT * FROM EVENTS;");
         while(rs.next()){
             event_list.add(new Circumstance(rs.getString("Name"), rs.getString("Date"), rs.getString("Subject"), rs.getString("CharacterID"), rs.getString("EventID")));
         }
-        because();
+        closeConnection();
     }
 
 
     public void updateChar(Character c) throws SQLException {
-        // Name VARCHAR, Age VARCHAR, Gender VARCHAR, Race VARCHAR, Occupation VARCHAR, CharacterID VARCHAR
-        why();
+        openConnection();
     	String cmd = "UPDATE CHARACTERS SET Name ='" + c.getName() + "', Age = '" + c.getAge() + "', Gender = '" +
                 c.getGender() + "', Race = '" + c.getRace() + "', Occupation = '" + c.getOccupation() +
                 "', CharacterID = '" + c.getcID() + "' WHERE CharacterID = '" + c.getcID() + "';";
         execute(cmd);
-        because();
+        closeConnection();
     }
     public void updateItem(Item i) throws SQLException {
         String cmd = "UPDATE ITEMS SET Name = '" + i.getName() + "', Location = '" + i.getLocation() + "', Owner = '" + i.getOwner() + "', CharacterID = '" + i.getoID() + "' WHERE ItemID = '" + i.getiID() + "';";
         execute(cmd);
     }
     public void updateEvent(Circumstance e) throws SQLException{
-    	System.out.println("sID: " + e.getsID());
     	String cmd = "UPDATE EVENTS SET Name ='" + e.getName() + "', Date = '" + e.getDate() + "', Subject = '" + e.getSubject() + "', CharacterID = '" + e.getsID() + "' WHERE EventID = '" + e.geteID() + "';";
         execute(cmd);
     }
     public void addChar(Character c) throws SQLException {
-        // instead of item. we could add each of the column inputs. it may be better that way.
-        // (Name, Gender, Age, Race, Occupation, CharacterID)
         character_list.add(c);
         String cmd = "INSERT INTO CHARACTERS VALUES ('" + c.getName() + "','"
                 + c.getGender() + "','" + c.getAge() + "','" + c.getRace() + "','" + c.getOccupation() + "','" + c.getcID() + "' );";
@@ -113,33 +107,20 @@ public class Database {
                 event.getName() + "','" + event.getDate() + "','" + event.getSubject() +"','" + event.getsID() + "','" + event.geteID() + "');";
         execute(cmd);
     }
-    public void deleteItem(Item item) throws SQLException {
-        String cmd = "DELETE from ITEMS where ItemID=" + item.getiID() + ";";
-        item_list.remove(item);
-        execute(cmd);
-    }
     public void deleteChar(Character chr) throws SQLException {
         String cmd = "DELETE from CHARACTERS where CharacterID=" + chr.getcID() + ";";
         character_list.remove(chr);
+        execute(cmd);
+    }
+    public void deleteItem(Item item) throws SQLException {
+        String cmd = "DELETE from ITEMS where ItemID=" + item.getiID() + ";";
+        item_list.remove(item);
         execute(cmd);
     }
     public void deleteEvent(Circumstance circ) throws SQLException {
         String cmd = "DELETE from EVENTS where EventID=" + circ.geteID() + ";";
         event_list.remove(circ);
         execute(cmd);
-    }
-    private Item findItem(String ID){
-        Item item = null;
-        for (int i = 0; i < item_list.size(); i++) {
-            if (ID.equals(item_list.get(i).getiID())) {
-                item = item_list.get(i);
-                break;
-            }
-        }
-        if (item == null){
-            throw new NullPointerException();
-        }
-        return item;
     }
     private Character findChar(String ID){
         Character charchanging =  null; // NO OTHER OPTION
@@ -154,6 +135,19 @@ public class Database {
         }
         return charchanging;
     }
+    private Item findItem(String ID) {
+        Item item = null;
+        for (int i = 0; i < item_list.size(); i++) {
+            if (ID.equals(item_list.get(i).getiID())) {
+                item = item_list.get(i);
+                break;
+            }
+        }
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        return item;
+    }
     private Circumstance findEvent(String ID){
         Circumstance event = null;
         for (int i = 0; i < event_list.size(); i++){
@@ -167,10 +161,50 @@ public class Database {
         }
         return event;
     }
+    public void queryChar(String Column, String input) throws SQLException {
+        openConnection();
+        filter_character.clear();
+        ResultSet rs = stat.executeQuery( "SELECT * FROM CHARACTERS;" );
+        int in_len = input.length();
+        while (rs.next()){
+            int search_len = in_len -1;
+            if (rs.getString(Column).length() <= search_len){
+                search_len = rs.getString(Column).length();
+            }
+            if (input.length() <= rs.getString(Column).length()){
+                if (rs.getString(Column).toLowerCase().substring(0,search_len+1).equals(input.toLowerCase())){
+                    System.out.println(rs.getString("CharacterID"));
+                    filter_character.add(findChar(rs.getString("CharacterID")));
+                }
+            }
 
+        }
+        closeConnection();
+    }
+    public void queryitem(String Column, String input) throws SQLException{
+        openConnection();
+        filter_items.clear();
+        System.out.println(Column);
+        System.out.println(input);
+        ResultSet rs = stat.executeQuery("SELECT * FROM ITEMS;");
+        int in_len = input.length();
+        while (rs.next()){
+            int search_len = in_len -1;
+            if (rs.getString(Column).length() <= search_len){
+                search_len = rs.getString(Column).length();
+            }
+            if (input.length() <= rs.getString(Column).length()){
+                if (rs.getString(Column).toLowerCase().substring(0, search_len+1).equals(input.toLowerCase())){
+                    System.out.println(rs.getString("ItemID"));
+                    filter_items.add(findItem(rs.getString("ItemID")));
+                }
+            }
+        }
+        closeConnection();
+    }
     public void queryEvent(String Column, String input) throws SQLException {
     	System.out.println("THis is now LJ's controller not calling it");
-        why();
+        openConnection();
         filter_Event.clear();
         ResultSet rs = stat.executeQuery( "SELECT * FROM EVENTS;" );
         int in_len = input.length();
@@ -188,93 +222,51 @@ public class Database {
                 }
             }
         }
-        because();
+        closeConnection();
     }
-    public void queryitem(String Column, String input) throws SQLException{
-        why();
-        filter_items.clear();
-        System.out.println(Column);
-        System.out.println(input);
-        ResultSet rs = stat.executeQuery("SELECT * FROM ITEMS;");
-        int in_len = input.length();
-        while (rs.next()){
-        	int search_len = in_len -1;
-        	if (rs.getString(Column).length() <= search_len){
-        		search_len = rs.getString(Column).length();
-        	}
-            if (input.length() <= rs.getString(Column).length()){
-                if (rs.getString(Column).toLowerCase().substring(0, search_len+1).equals(input.toLowerCase())){
-                    System.out.println(rs.getString("ItemID"));
-                    filter_items.add(findItem(rs.getString("ItemID")));
-                }
-            }
-        }
-        because();
-    }
-    public void queryChar(String Column, String input) throws SQLException {
-        why();
-    	filter_character.clear();
-        ResultSet rs = stat.executeQuery( "SELECT * FROM CHARACTERS;" );
-        int in_len = input.length();
-        while (rs.next()){
-        	int search_len = in_len -1;
-        	if (rs.getString(Column).length() <= search_len){
-        		search_len = rs.getString(Column).length();
-        	}
-            if (input.length() <= rs.getString(Column).length()){
-                if (rs.getString(Column).toLowerCase().substring(0,search_len+1).equals(input.toLowerCase())){
-                    System.out.println(rs.getString("CharacterID"));
-                    filter_character.add(findChar(rs.getString("CharacterID")));
-                }
-            }
 
-        }
-        because();
-    }
+
     public String getMaxcID() throws SQLException {
-        why();
+        openConnection();
         if (character_list.size() > 0){
             ResultSet rs = stat.executeQuery( "SELECT * FROM CHARACTERS ORDER BY CharacterID DESC LIMIT 1;" );
             return rs.getString("CharacterID");
         }
-        because();
+        closeConnection();
         return "0";
 
     }
     public String getMaxiID() throws SQLException {
-        why();
+        openConnection();
         if (item_list.size() > 0){
             ResultSet rs = stat.executeQuery( "SELECT * FROM ITEMS ORDER BY ItemID DESC LIMIT 1;" );
             return rs.getString("ItemID");
         }
-        because();
+        closeConnection();
         return "0";
     }
     public String getMaxeID() throws SQLException {
-        why();
+        openConnection();
         if (item_list.size() > 0){
-            ResultSet rs = stat.executeQuery( "SELECT * FROM EVENT ORDER BY EventID DESC LIMIT 1;" );
+            ResultSet rs = stat.executeQuery( "SELECT * FROM EVENTS ORDER BY EventID DESC LIMIT 1;" );
             return rs.getString("EventID");
         }
-        because();
+        closeConnection();
         return "0";
     }
 
 
     private void execute(String cmd) throws SQLException {
-        why();
+        openConnection();
         System.out.println(cmd);
         stat.execute(cmd);
-        because();
+        closeConnection();
     }
-    public ObservableList<Circumstance> getFilter_Event(){return filter_Event;}
-    public ObservableList<Item> getFilter_Items(){return filter_items;}
-    public ObservableList<Character> getFilter_Character(){return filter_character;}
-    public ObservableList<Item> getItem_list(){return item_list;}
     public ObservableList<Character> getCharacter_list(){return character_list;}
+    public ObservableList<Item> getItem_list(){return item_list;}
     public ObservableList<Circumstance> getEvent_list(){return event_list;}
-
-
-
+    public ObservableList<Character> getFilter_Character(){return filter_character;}
+    public ObservableList<Item> getFilter_Items(){return filter_items;}
+    public ObservableList<Circumstance> getFilter_Event(){return filter_Event;}
 
 }
